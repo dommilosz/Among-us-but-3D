7b46e632456fd3f0f679ef0710b0b0c17517e9f5
dommilosz <dommilosz@gmail.com>
2020-11-05 20:01:15 +0100

    ta Multiplayer niby dziala ale nie

---
+++ Assets/Multiplayer Test/AmongUsGameManager.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmongUsGameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate("PlayerV2", transform.position, transform.rotation, 0);      // avoid this call on rejoin (ship was network instantiated before)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
